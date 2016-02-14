684cc06a72cb7816c87bd1d4ae7d7d38c1e8dd01
Nicholas Vaughan <nick@HAL2000.Nicks_domain>
2016-02-13 18:46:42 -0600

    This is just a rough program to test if it worked.

--- test14.cpp
+++ test14.cpp
@@ -7,9 +7,13 @@ using namespace std;
 int main()
 {
      string name;
+     int width;
+
      cout << "What is your name? ";
      getline (cin, name);
-     int width = (30 - name.length())/2;
+
+     width = (30 - name.length())/2;
+
      cout << setw(width) << setfill('-') << "-";
      cout << left << setw(30 - width) << name << endl;
 }
